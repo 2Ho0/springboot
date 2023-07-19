ff2fd7a0e3d8619de438fc093df503e318d632b8
leehoyoeung <mwilliam55@naver.com>
2023-07-19 23:21:39 +0900

    4.2

---
+++ src/main/java/_202307start/web/IndexController.java
@@ -0,0 +1,14 @@
+package _202307start.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
