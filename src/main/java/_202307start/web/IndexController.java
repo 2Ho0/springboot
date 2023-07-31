56e33f6ac3311403710e8f92f5c8fa8959ef3068
leehoyoeung <mwilliam55@naver.com>
2023-07-31 13:37:13 +0900

    4.5

--- src/main/java/_202307start/web/IndexController.java
+++ src/main/java/_202307start/web/IndexController.java
@@ -1,14 +1,35 @@
 package _202307start.web;
 
+import _202307start.service.posts.PostsService;
+import _202307start.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
     }
 
 }
