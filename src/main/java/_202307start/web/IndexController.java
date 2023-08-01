41ec83d7fae7f72eefaf8d3ec4682878068c9f62
leehoyoeung <mwilliam55@naver.com>
2023-08-01 23:31:29 +0900

    5.3

--- src/main/java/_202307start/web/IndexController.java
+++ src/main/java/_202307start/web/IndexController.java
@@ -1,5 +1,6 @@
 package _202307start.web;
 
+import _202307start.config.auth.dto.SessionUser;
 import _202307start.service.posts.PostsService;
 import _202307start.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
