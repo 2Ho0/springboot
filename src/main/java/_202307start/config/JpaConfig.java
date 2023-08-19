5dcb938a7a840db0ac1d7546d057e72595f97c27
leehoyoeung <mwilliam55@naver.com>
2023-08-19 21:55:33 +0900

    5.7

---
+++ src/main/java/_202307start/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package _202307start.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/main/java/_202307start/web/IndexController.java
+++ src/main/java/_202307start/web/IndexController.java
@@ -1,5 +1,6 @@
 package _202307start.web;
 
+import _202307start.config.auth.LoginUser;
 import _202307start.config.auth.dto.SessionUser;
 import _202307start.service.posts.PostsService;
 import _202307start.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
